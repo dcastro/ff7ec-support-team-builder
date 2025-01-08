3197bf88132785f962ea804bd174f38706db1299
Diogo Castro <dc@diogocastro.com>
2025-01-08 14:31:45 +0000

    Added `Search.assignWeaponsToCharacters`

--- src/Core/Weapons/Search.purs
+++ src/Core/Weapons/Search.purs
@@ -2,11 +2,16 @@ module Core.Weapons.Search where
 
 import Core.Weapons.Types
 import Prelude
+
 import Data.Array as Arr
 import Data.Foldable as F
 import Data.Generic.Rep (class Generic)
+import Data.Map (Map)
+import Data.Map as Map
 import Data.Maybe (Maybe(..))
 import Data.Show.Generic (genericShow)
+import Data.String.NonEmpty (NonEmptyString)
+import Data.String.NonEmpty as NES
 import Unsafe.Coerce (unsafeCoerce)
 import Yoga.JSON (class WriteForeign)
 import Yoga.JSON.Generics as J
@@ -152,6 +157,94 @@ matches filter weapon =
     FilterSelfOrSingleTargetOrAll, Self -> true
     FilterSelfOrSingleTargetOrAll, SingleTarget -> true
     FilterSelfOrSingleTargetOrAll, All -> true
+
+type Character =
+  { name :: NonEmptyString
+  , mainHand :: EquipedWeapon
+  , offHand :: Maybe EquipedWeapon
+  }
+
+type EquipedWeapon =
+  { weapon :: Weapon
+  -- The filters that this weapon matched on.
+  , matchedFilters :: Array Filter
+  }
+
+type AssignmentResult =
+  { -- | Characters indexed by their name.
+    characters :: Map String Character
+  ,
+    -- | A list of the effects for which no weapon was found.
+    missedFilters :: Array Filter
+  }
+
+-- Attempts to equip the selected weapons to, at maximum, `n` characters.
+--
+-- Returns `Nothing` if:
+--  * There are more than 2 weapons for any character.
+--  * The selected weapons belong to more than `n` characters.
+assignWeaponsToCharacters :: Int -> Combination -> Maybe AssignmentResult
+assignWeaponsToCharacters maxCharacterCount =
+  Arr.foldRecM
+    ( \assignments { filter, weapon } ->
+        case weapon of
+          Nothing ->
+            -- No weapons found for this filter
+            Just $ assignments { missedFilters = Arr.cons filter assignments.missedFilters }
+          Just weapon -> do
+            let
+              characterName = weapon.character
+              characterName' = NES.toString characterName
+            updatedCharacters <-
+              case Map.lookup characterName' assignments.characters of
+                Nothing ->
+                  -- This character hasn't been created yet, so we attempt to create it.
+                  if Map.size assignments.characters >= maxCharacterCount then Nothing
+                  else Just $ Map.insert characterName' (mkCharacter characterName weapon filter) assignments.characters
+                Just existingCharacter -> do
+                  -- This character already exists, so we attempt to equip this weapon.
+                  updatedCharacter <- equipWeapon weapon filter existingCharacter
+                  pure $ Map.insert characterName' updatedCharacter assignments.characters
+            Just $ assignments { characters = updatedCharacters }
+    )
+    { characters: Map.empty, missedFilters: [] }
+
+  where
+
+  mkCharacter :: NonEmptyString -> Weapon -> Filter -> Character
+  mkCharacter name mainHandWeapon matchedFilter =
+    { name
+    , mainHand:
+        { weapon: mainHandWeapon
+        , matchedFilters: [ matchedFilter ]
+        }
+    , offHand: Nothing
+    }
+
+  equipWeapon :: Weapon -> Filter -> Character -> Maybe Character
+  equipWeapon weapon filter character = do
+    -- If this weapon is already equiped in the main hand, simply update `matchedFilters`
+    if character.mainHand.weapon.name == weapon.name then Just character { mainHand = addMatchedFilter filter character.mainHand }
+    else
+      case character.offHand of
+        Just offHand | offHand.weapon.name == weapon.name -> do
+          -- If this weapon is already equiped in the off hand, simply update `matchedFilters`
+          pure $ character { offHand = Just $ addMatchedFilter filter offHand }
+        Just _ ->
+          -- If both hands are already equiped, we can't equip any more weapons - this function fails.
+          Nothing
+        Nothing ->
+          -- The off hand is free, we can equip this weapon in the off hand.
+          pure $ character
+            { offHand = Just
+                { weapon
+                , matchedFilters: [ filter ]
+                }
+            }
+
+  addMatchedFilter :: Filter -> EquipedWeapon -> EquipedWeapon
+  addMatchedFilter matchedFilter weapon =
+    weapon { matchedFilters = Arr.cons matchedFilter weapon.matchedFilters }
 
 defaultFilterRange :: FilterEffectType -> FilterRange
 defaultFilterRange = case _ of
--- test/Test/Core/Weapons/SearchSpec.purs
+++ test/Test/Core/Weapons/SearchSpec.purs
@@ -1,151 +1,279 @@
-module Test.Core.Weapons.SearchSpec where
+module Test.Core.Weapons.SearchSpec (spec) where
 
 import Prelude
 import Test.Spec
+
 import Core.Weapons.Search (FilterEffectType(..), FilterRange(..), Filter)
 import Core.Weapons.Search as Search
 import Core.Weapons.Types (Weapon)
+import Data.Map as Map
 import Data.Maybe (Maybe(..))
+import Data.Nullable (null)
+import Data.Nullable as Null
 import Data.String.NonEmpty (NonEmptyString)
+import Data.Tuple (Tuple(..))
 import Test.Utils (nes, shouldEqualPretty)
 
 spec :: Spec Unit
 spec =
   describe "search" do
-    describe "combinations" do
-      it "finds all possible combinations" do
-        let
-          combs = Search.combinations
-            [ { filter: filter1
-              , required: true
-              , matchingWeapons:
-                  [ weapon11
-                  , weapon12
-                  , weapon13
-                  ]
-              }
-            , { filter: filter2
-              , required: true
-              , matchingWeapons:
-                  [ weapon21
-                  , weapon22
-                  ]
-              }
-            , { filter: filter3
-              , required: true
-              , matchingWeapons:
-                  [ weapon31
-                  ]
-              }
-            ]
+    combinationsSpec
+    assignWeaponsToCharactersSpec
 
-        combs `shouldEqualPretty`
-          [ [ { filter: filter1, weapon: Just weapon11 }
-            , { filter: filter2, weapon: Just weapon21 }
-            , { filter: filter3, weapon: Just weapon31 }
-            ]
-          , [ { filter: filter1, weapon: Just weapon11 }
-            , { filter: filter2, weapon: Just weapon22 }
-            , { filter: filter3, weapon: Just weapon31 }
-            ]
-          , [ { filter: filter1, weapon: Just weapon12 }
-            , { filter: filter2, weapon: Just weapon21 }
-            , { filter: filter3, weapon: Just weapon31 }
-            ]
-          , [ { filter: filter1, weapon: Just weapon12 }
-            , { filter: filter2, weapon: Just weapon22 }
-            , { filter: filter3, weapon: Just weapon31 }
-            ]
-          , [ { filter: filter1, weapon: Just weapon13 }
-            , { filter: filter2, weapon: Just weapon21 }
-            , { filter: filter3, weapon: Just weapon31 }
-            ]
-          , [ { filter: filter1, weapon: Just weapon13 }
-            , { filter: filter2, weapon: Just weapon22 }
-            , { filter: filter3, weapon: Just weapon31 }
-            ]
-          ]
-      it "returns no combinations when a match is not found for a required effect" do
-        let
-          combs = Search.combinations
-            [ { filter: filter1
-              , required: true
-              , matchingWeapons:
-                  [ weapon11
-                  , weapon12
-                  ]
-              }
-            , { filter: filter2
-              , required: true
-              , matchingWeapons:
-                  [ weapon21
-                  , weapon22
-                  ]
-              }
-            , { filter: filter3
-              , required: true
-              , matchingWeapons:
-                  [
-                  ]
-              }
-            ]
-        combs `shouldEqualPretty` []
-      it "returns partial combinations when a match is not found for an optional effect" do
-        let
-          combs = Search.combinations
-            [ { filter: filter1
-              , required: true
-              , matchingWeapons:
-                  [ weapon11
-                  , weapon12
-                  , weapon13
-                  ]
-              }
-            , { filter: filter2
-              , required: true
-              , matchingWeapons:
-                  [ weapon21
-                  , weapon22
-                  ]
-              }
-            , { filter: filter3
-              , required: false
-              , matchingWeapons:
-                  [
-                  ]
-              }
-            ]
-        combs `shouldEqualPretty`
-          [ [ { filter: filter1, weapon: Just weapon11 }
-            , { filter: filter2, weapon: Just weapon21 }
-            , { filter: filter3, weapon: Nothing }
-            ]
-          , [ { filter: filter1, weapon: Just weapon11 }
-            , { filter: filter2, weapon: Just weapon22 }
-            , { filter: filter3, weapon: Nothing }
-            ]
-          , [ { filter: filter1, weapon: Just weapon12 }
-            , { filter: filter2, weapon: Just weapon21 }
-            , { filter: filter3, weapon: Nothing }
-            ]
-          , [ { filter: filter1, weapon: Just weapon12 }
-            , { filter: filter2, weapon: Just weapon22 }
-            , { filter: filter3, weapon: Nothing }
-            ]
-          , [ { filter: filter1, weapon: Just weapon13 }
-            , { filter: filter2, weapon: Just weapon21 }
-            , { filter: filter3, weapon: Nothing }
-            ]
-          , [ { filter: filter1, weapon: Just weapon13 }
-            , { filter: filter2, weapon: Just weapon22 }
-            , { filter: filter3, weapon: Nothing }
-            ]
+combinationsSpec :: Spec Unit
+combinationsSpec = do
+  describe "combinations" do
+    let
+      weapon11 = mkWeapon (nes @"11") (nes @" ")
+      weapon12 = mkWeapon (nes @"12") (nes @" ")
+      weapon13 = mkWeapon (nes @"13") (nes @" ")
+      weapon21 = mkWeapon (nes @"21") (nes @" ")
+      weapon22 = mkWeapon (nes @"22") (nes @" ")
+      weapon31 = mkWeapon (nes @"31") (nes @" ")
+
+    it "finds all possible combinations" do
+      let
+        combs = Search.combinations
+          [ { filter: filter1
+            , required: true
+            , matchingWeapons:
+                [ weapon11
+                , weapon12
+                , weapon13
+                ]
+            }
+          , { filter: filter2
+            , required: true
+            , matchingWeapons:
+                [ weapon21
+                , weapon22
+                ]
+            }
+          , { filter: filter3
+            , required: true
+            , matchingWeapons:
+                [ weapon31
+                ]
+            }
           ]
 
-mkWeapon :: NonEmptyString -> Weapon
-mkWeapon id =
+      combs `shouldEqualPretty`
+        [ [ { filter: filter1, weapon: Just weapon11 }
+          , { filter: filter2, weapon: Just weapon21 }
+          , { filter: filter3, weapon: Just weapon31 }
+          ]
+        , [ { filter: filter1, weapon: Just weapon11 }
+          , { filter: filter2, weapon: Just weapon22 }
+          , { filter: filter3, weapon: Just weapon31 }
+          ]
+        , [ { filter: filter1, weapon: Just weapon12 }
+          , { filter: filter2, weapon: Just weapon21 }
+          , { filter: filter3, weapon: Just weapon31 }
+          ]
+        , [ { filter: filter1, weapon: Just weapon12 }
+          , { filter: filter2, weapon: Just weapon22 }
+          , { filter: filter3, weapon: Just weapon31 }
+          ]
+        , [ { filter: filter1, weapon: Just weapon13 }
+          , { filter: filter2, weapon: Just weapon21 }
+          , { filter: filter3, weapon: Just weapon31 }
+          ]
+        , [ { filter: filter1, weapon: Just weapon13 }
+          , { filter: filter2, weapon: Just weapon22 }
+          , { filter: filter3, weapon: Just weapon31 }
+          ]
+        ]
+    it "returns no combinations when a match is not found for a required effect" do
+      let
+        combs = Search.combinations
+          [ { filter: filter1
+            , required: true
+            , matchingWeapons:
+                [ weapon11
+                , weapon12
+                ]
+            }
+          , { filter: filter2
+            , required: true
+            , matchingWeapons:
+                [ weapon21
+                , weapon22
+                ]
+            }
+          , { filter: filter3
+            , required: true
+            , matchingWeapons:
+                [
+                ]
+            }
+          ]
+      combs `shouldEqualPretty` []
+    it "returns partial combinations when a match is not found for an optional effect" do
+      let
+        combs = Search.combinations
+          [ { filter: filter1
+            , required: true
+            , matchingWeapons:
+                [ weapon11
+                , weapon12
+                , weapon13
+                ]
+            }
+          , { filter: filter2
+            , required: true
+            , matchingWeapons:
+                [ weapon21
+                , weapon22
+                ]
+            }
+          , { filter: filter3
+            , required: false
+            , matchingWeapons:
+                [
+                ]
+            }
+          ]
+      combs `shouldEqualPretty`
+        [ [ { filter: filter1, weapon: Just weapon11 }
+          , { filter: filter2, weapon: Just weapon21 }
+          , { filter: filter3, weapon: Nothing }
+          ]
+        , [ { filter: filter1, weapon: Just weapon11 }
+          , { filter: filter2, weapon: Just weapon22 }
+          , { filter: filter3, weapon: Nothing }
+          ]
+        , [ { filter: filter1, weapon: Just weapon12 }
+          , { filter: filter2, weapon: Just weapon21 }
+          , { filter: filter3, weapon: Nothing }
+          ]
+        , [ { filter: filter1, weapon: Just weapon12 }
+          , { filter: filter2, weapon: Just weapon22 }
+          , { filter: filter3, weapon: Nothing }
+          ]
+        , [ { filter: filter1, weapon: Just weapon13 }
+          , { filter: filter2, weapon: Just weapon21 }
+          , { filter: filter3, weapon: Nothing }
+          ]
+        , [ { filter: filter1, weapon: Just weapon13 }
+          , { filter: filter2, weapon: Just weapon22 }
+          , { filter: filter3, weapon: Nothing }
+          ]
+        ]
+
+assignWeaponsToCharactersSpec :: Spec Unit
+assignWeaponsToCharactersSpec = do
+  describe "assignWeaponsToCharacters" do
+    let tifaWeapon1 = mkWeapon (nes @"Tifa 1") (nes @"Tifa")
+    let tifaWeapon2 = mkWeapon (nes @"Tifa 2") (nes @"Tifa")
+    let tifaWeapon3 = mkWeapon (nes @"Tifa 3") (nes @"Tifa")
+
+    let vincentWeapon1 = mkWeapon (nes @"Vincent 1") (nes @"Vincent")
+    let vincentWeapon2 = mkWeapon (nes @"Vincent 2") (nes @"Vincent")
+
+    let redWeapon1 = mkWeapon (nes @"Red XIII 1") (nes @"Red XIII")
+
+    it "assigns weapons correctly" do
+      let
+        combination =
+          [ { filter: filter1, weapon: Just tifaWeapon1 }
+          , { filter: filter2, weapon: Just tifaWeapon2 }
+          , { filter: filter3, weapon: Just vincentWeapon1 }
+          , { filter: filter4, weapon: Just vincentWeapon2 }
+          ]
+
+      Search.assignWeaponsToCharacters 3 combination `shouldEqualPretty`
+        Just
+          { characters:
+              ( Map.fromFoldable
+                  [ Tuple "Tifa"
+                      { name: nes @"Tifa"
+                      , mainHand:
+                          { weapon: tifaWeapon1
+                          , matchedFilters: [ filter1 ]
+                          }
+                      , offHand: Just
+                          { weapon: tifaWeapon2
+                          , matchedFilters: [ filter2 ]
+                          }
+                      }
+                  , Tuple "Vincent"
+                      { name: nes @"Vincent"
+                      , mainHand:
+                          { weapon: vincentWeapon1
+                          , matchedFilters: [ filter3 ]
+                          }
+                      , offHand: Just
+                          { weapon: vincentWeapon2
+                          , matchedFilters: [ filter4 ]
+                          }
+                      }
+                  ]
+              )
+          , missedFilters: []
+          }
+
+    it "handles a weapon matching on 2 or more effects" do
+      let
+        combination =
+          [ { filter: filter1, weapon: Just tifaWeapon1 }
+          , { filter: filter2, weapon: Just tifaWeapon1 }
+          , { filter: filter3, weapon: Just tifaWeapon2 }
+          , { filter: filter4, weapon: Just tifaWeapon2 }
+          , { filter: filter5, weapon: Just vincentWeapon1 }
+          ]
+
+      Search.assignWeaponsToCharacters 3 combination `shouldEqualPretty`
+        Just
+          { characters:
+              ( Map.fromFoldable
+                  [ Tuple "Tifa"
+                      { name: nes @"Tifa"
+                      , mainHand:
+                          { weapon: tifaWeapon1
+                          , matchedFilters: [ filter2, filter1 ]
+                          }
+                      , offHand: Just
+                          { weapon: tifaWeapon2
+                          , matchedFilters: [ filter4, filter3 ]
+                          }
+                      }
+                  , Tuple "Vincent"
+                      { name: nes @"Vincent"
+                      , mainHand:
+                          { weapon: vincentWeapon1
+                          , matchedFilters: [ filter5 ]
+                          }
+                      , offHand: Nothing
+                      }
+                  ]
+              )
+          , missedFilters: []
+          }
+
+    it "fails if more than 2 weapons were selected for the same character" do
+      let
+        combination =
+          [ { filter: filter1, weapon: Just tifaWeapon1 }
+          , { filter: filter2, weapon: Just tifaWeapon2 }
+          , { filter: filter3, weapon: Just tifaWeapon3 }
+          ]
+
+      Null.toNullable (Search.assignWeaponsToCharacters 3 combination) `shouldEqualPretty` null
+
+    it "fails if more than 2 characters were selected" do
+      let
+        combination =
+          [ { filter: filter1, weapon: Just tifaWeapon1 }
+          , { filter: filter2, weapon: Just vincentWeapon1 }
+          , { filter: filter3, weapon: Just redWeapon1 }
+          ]
+
+      Null.toNullable (Search.assignWeaponsToCharacters 2 combination) `shouldEqualPretty` null
+
+mkWeapon :: NonEmptyString -> NonEmptyString -> Weapon
+mkWeapon id character =
   { name: id
-  , character: nes @" "
+  , character
   , image: nes @" "
   , ob0: { description: nes @" ", effects: [] }
   , ob1: { description: nes @" ", effects: [] }
@@ -160,27 +288,11 @@ filter1 =
   , range: FilterAll
   }
 
-weapon11 :: Weapon
-
-weapon11 = mkWeapon $ nes @"11"
-
-weapon12 :: Weapon
-weapon12 = mkWeapon $ nes @"12"
-
-weapon13 :: Weapon
-weapon13 = mkWeapon $ nes @"13"
-
 filter2 :: Filter
 filter2 =
   { effectType: FilterPatkDown
   , range: FilterSingleTargetOrAll
   }
-
-weapon21 :: Weapon
-weapon21 = mkWeapon $ nes @"21"
-
-weapon22 :: Weapon
-weapon22 = mkWeapon $ nes @"22"
 
 filter3 :: Filter
 filter3 =
@@ -188,5 +300,14 @@ filter3 =
   , range: FilterSelfOrSingleTargetOrAll
   }
 
-weapon31 :: Weapon
-weapon31 = mkWeapon $ nes @"31"
+filter4 :: Filter
+filter4 =
+  { effectType: FilterVeil
+  , range: FilterSelfOrSingleTargetOrAll
+  }
+
+filter5 :: Filter
+filter5 =
+  { effectType: FilterIceResistDown
+  , range: FilterAll
+  }

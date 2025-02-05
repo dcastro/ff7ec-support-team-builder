aae9fbdafb0a038dd7ce6f0638c1014c76414f9a
Diogo Castro <dc@diogocastro.com>
2025-02-05 22:37:11 +0000

    Started db rewrite

---
+++ src/Core/Database.purs
@@ -0,0 +1,294 @@
+module Core.Database (init) where
+
+import Core.Database.VLatest2
+import Prelude
+
+import Control.Monad.Error.Class (class MonadThrow, throwError)
+import Control.Monad.Except (runExceptT)
+import Control.Monad.Rec.Class (class MonadRec)
+import Core.Display (display)
+import Core.Weapons.Parser2 as P
+import Core.WebStorage as WS
+import Data.Array as Arr
+import Data.Array.NonEmpty (NonEmptyArray)
+import Data.DateTime (DateTime)
+import Data.DateTime as DateTime
+import Data.Either (Either(..), hush)
+import Data.Foldable as F
+import Data.List.Lazy as LazyList
+import Data.List.ZipList (ZipList(..))
+import Data.Map (Map)
+import Data.Map as Map
+import Data.Maybe (Maybe(..))
+import Data.Newtype (unwrap)
+import Data.Set as Set
+import Data.Time.Duration (Hours(..))
+import Data.Traversable (for_)
+import Effect.Aff (Aff)
+import Effect.Aff.Class (class MonadAff, liftAff)
+import Effect.Class (class MonadEffect, liftEffect)
+import Effect.Class.Console as Console
+import Effect.Now as Now
+import Google.SheetsApi as SheetsApi
+import Unsafe.Coerce (unsafeCoerce)
+import Utils (MapAsArray(..), SetAsArray(..), logOnLeft, renderJsonErr, throwOnNothing, whenJust)
+import Yoga.JSON as J
+
+currentDbVersion :: Int
+currentDbVersion = 1
+
+init :: Aff (Maybe Db)
+init = do
+  runExceptT readFromCache >>= case _ of
+    Left _ -> do
+      Console.log "Db not found in cache, loading it from the spreadsheet..."
+      dbMb <- hush <$> runExceptT (loadAndCreateDb Map.empty)
+      whenJust dbMb $ writeToCache
+      pure dbMb
+    Right { db, hasExpired } | hasExpired -> do
+      Console.log "Db found in cache but has expired, updating cache..."
+      hush <$> runExceptT (loadAndCreateDb db.allWeapons) >>= case _ of
+        Just updatedDb -> do
+          writeToCache updatedDb
+          pure $ Just updatedDb
+        Nothing -> do
+          Console.log "Failed to update db"
+          pure $ Just db
+    Right { db, hasExpired: _ } -> do
+      Console.log "Db found in cache."
+      pure $ Just db
+
+  where
+  -- Load the weapons from the spreadsheet, and updates the existing db.
+  loadAndCreateDb :: forall f. MonadAff f => MonadThrow Unit f => MonadRec f => Map WeaponName WeaponData -> f Db
+  loadAndCreateDb existingWeapons = do
+    weapons <- loadFromSpreadsheet
+    createDb weapons existingWeapons
+
+  -- Throws if we can't parse the Google Sheet.
+  loadFromSpreadsheet :: forall f. MonadAff f => MonadThrow Unit f => f (Array Weapon)
+  loadFromSpreadsheet = do
+    table <- liftAff $ SheetsApi.getSheet "Weapons!A:Z"
+
+    let { weapons, errors } = P.parseWeapons table.result.values
+    for_ errors \err -> Console.log $ "Failed to parse weapon:\n" <> err
+
+    when (Arr.null weapons) do
+      Console.log "Failed to parse any weapons"
+      throwError unit
+    pure weapons
+
+getDistinctObs :: Weapon -> NonEmptyArray ObRange
+getDistinctObs _ = do
+  unsafeCoerce "TODO"
+
+pickOb6 :: NonEmptyArray ObRange -> ObRange
+pickOb6 _ = do
+  unsafeCoerce "TODO"
+
+pickOb :: ObRange -> NonEmptyArray ObRange -> ObRange
+pickOb _ = do
+  unsafeCoerce "TODO"
+
+createDb :: forall m. MonadEffect m => MonadRec m => Array Weapon -> Map WeaponName WeaponData -> m Db
+createDb newWeapons existingWeapons = do
+  Arr.foldRecM
+    (\db weapon -> insertWeapon weapon existingWeapons db)
+    newDb
+    newWeapons
+  where
+  newDb :: Db
+  newDb =
+    { allWeapons: Map.empty
+    , groupedByEffect: Map.empty
+    , allCharacterNames: Set.empty
+    }
+
+insertWeapon
+  :: forall m
+   . MonadEffect m
+  => Weapon
+  -> Map WeaponName WeaponData
+  -> Db
+  -> m Db
+insertWeapon weapon existingWeapons db =
+  case Map.lookup weapon.name existingWeapons of
+    Just existingWeapon -> do
+      Console.log $ "Weapon already exists, replacing it: " <> display weapon.name
+      pure $ db
+        # mergeWithExisting existingWeapon
+        # insertIntoGroups
+        # insertCharacterName
+    Nothing -> do
+      Console.log $ "Weapon added: " <> display weapon.name
+      pure $ db
+        # insert
+        # insertIntoGroups
+        # insertCharacterName
+  where
+
+  insert :: Db -> Db
+  insert db = do
+    let distinctObs = getDistinctObs weapon
+    let
+      newWeapon =
+        { weapon
+        , ignored: false
+        , distinctObs
+        , ownedOb: pickOb6 distinctObs
+        }
+    db { allWeapons = Map.insert weapon.name newWeapon db.allWeapons }
+
+  mergeWithExisting :: WeaponData -> Db -> Db
+  mergeWithExisting existing db = do
+    let distinctObs = getDistinctObs weapon
+    let
+      newWeapon =
+        { weapon
+        , ignored: existing.ignored
+        , distinctObs
+        , ownedOb: pickOb existing.ownedOb distinctObs
+        }
+    db { allWeapons = Map.insert weapon.name newWeapon db.allWeapons }
+
+  insertIntoGroups :: Db -> Db
+  insertIntoGroups db =
+    F.foldr
+      insertIntoGroup
+      db
+      (groupsForWeapon weapon)
+
+  insertIntoGroup :: GroupEntry -> Db -> Db
+  insertIntoGroup { effectType, groupedWeapon } db = do
+    let
+      groupedByEffect = Map.alter
+        ( case _ of
+            Just weapons -> Just $ Arr.snoc weapons groupedWeapon
+            Nothing -> Just [ groupedWeapon ]
+        )
+        effectType
+        db.groupedByEffect
+    db { groupedByEffect = groupedByEffect }
+
+  insertCharacterName :: Db -> Db
+  insertCharacterName db =
+    db { allCharacterNames = Set.insert weapon.character db.allCharacterNames }
+
+type GroupEntry =
+  { effectType :: FilterEffectType
+  , groupedWeapon :: GroupedWeapon
+  }
+
+groupsForWeapon :: Weapon -> LazyList.List GroupEntry
+groupsForWeapon weapon = do
+  LazyList.catMaybes $ unwrap groupsForWeapon'
+  where
+  groupsForWeapon' :: ZipList (Maybe GroupEntry)
+  groupsForWeapon' = ado
+    -- INVARIANT: this assumes weapon effects are listed in the same order at all overboost levels.
+    ob0 <- ZipList $ LazyList.fromFoldable weapon.ob0.effects
+    ob1 <- ZipList $ LazyList.fromFoldable weapon.ob1.effects
+    ob6 <- ZipList $ LazyList.fromFoldable weapon.ob6.effects
+    ob10 <- ZipList $ LazyList.fromFoldable weapon.ob10.effects
+    in
+      groupForWeaponEffect ob0 ob1 ob6 ob10 <#> \{ effectType, potencies } ->
+        { effectType
+        , groupedWeapon:
+            { weaponName: weapon.name
+            -- INVARIANT: this assumes an effect has the same range at all overboost levels.
+            , range: ob0.range
+            , potencies
+            }
+        }
+
+  groupForWeaponEffect
+    :: WeaponEffect
+    -> WeaponEffect
+    -> WeaponEffect
+    -> WeaponEffect
+    -> Maybe
+         { effectType :: FilterEffectType
+         , potencies :: Maybe GroupedWeaponPotencies
+         }
+  groupForWeaponEffect ob0 ob1 ob6 ob10 = do
+    case ob0.effectType of
+      Heal { percentage } ->
+        if unwrap percentage >= 35 then Just { effectType: FilterHeal, potencies: Nothing }
+        else Nothing
+      Veil _ -> Just { effectType: FilterVeil, potencies: Nothing }
+      Provoke _ -> Just { effectType: FilterProvoke, potencies: Nothing }
+      Enfeeble _ -> Just { effectType: FilterEnfeeble, potencies: Nothing }
+      Stop _ -> Just { effectType: FilterStop, potencies: Nothing }
+      ExploitWeakness _ -> Just { effectType: FilterExploitWeakness, potencies: Nothing }
+      PatkUp { potencies: ob0Potencies } ->
+        case ob1.effectType, ob6.effectType, ob10.effectType of
+          PatkUp ob1, PatkUp ob6, PatkUp ob10 ->
+            Just
+              { effectType: FilterPatkUp
+              , potencies: Just
+                  { ob0: ob0Potencies
+                  , ob1: ob1.potencies
+                  , ob6: ob6.potencies
+                  , ob10: ob10.potencies
+                  }
+              }
+          _, _, _ -> unsafeCoerce "TODO"
+      MatkUp {} -> unsafeCoerce "TODO"
+      PdefUp {} -> unsafeCoerce "TODO"
+      MdefUp {} -> unsafeCoerce "TODO"
+      FireDamageUp {} -> unsafeCoerce "TODO"
+      IceDamageUp {} -> unsafeCoerce "TODO"
+      ThunderDamageUp {} -> unsafeCoerce "TODO"
+      EarthDamageUp {} -> unsafeCoerce "TODO"
+      WaterDamageUp {} -> unsafeCoerce "TODO"
+      WindDamageUp {} -> unsafeCoerce "TODO"
+      PatkDown {} -> unsafeCoerce "TODO"
+      MatkDown {} -> unsafeCoerce "TODO"
+      PdefDown {} -> unsafeCoerce "TODO"
+      MdefDown {} -> unsafeCoerce "TODO"
+      FireResistDown {} -> unsafeCoerce "TODO"
+      IceResistDown {} -> unsafeCoerce "TODO"
+      ThunderResistDown {} -> unsafeCoerce "TODO"
+      EarthResistDown {} -> unsafeCoerce "TODO"
+      WaterResistDown {} -> unsafeCoerce "TODO"
+      WindResistDown {} -> unsafeCoerce "TODO"
+
+-- Throws if the cache is empty OR the cache data is corrupted.
+readFromCache :: forall m. MonadThrow Unit m => MonadAff m => m { db :: Db, hasExpired :: Boolean }
+readFromCache = do
+  dbStr <- throwOnNothing $ WS.getItem "db"
+  lastUpdatedStr <- throwOnNothing $ WS.getItem "last_updated"
+
+  db :: Db <- fromSerializable <$> J.readJSON dbStr `logOnLeft` \err ->
+    "Failed to deserialize db:\n" <> renderJsonErr err
+  lastUpdated :: DateTime <- J.readJSON lastUpdatedStr `logOnLeft` \err ->
+    "Failed to deserialize db:\n" <> renderJsonErr err
+
+  now <- liftEffect Now.nowDateTime
+  let hasExpired = DateTime.diff now lastUpdated > Hours 24.0
+
+  pure { db, hasExpired }
+  where
+  fromSerializable :: SerializableDb -> Db
+  fromSerializable db =
+    { allWeapons: unwrap db.allWeapons
+    , groupedByEffect: unwrap db.groupedByEffect
+    , allCharacterNames: unwrap db.allCharacterNames
+    }
+
+writeToCache :: forall m. MonadAff m => Db -> m Unit
+writeToCache db = do
+  let dbStr = J.writeJSON $ toSerializable db
+  lastUpdatedStr <- J.writeJSON <$> liftEffect Now.nowDateTime
+  let currentDbVersionStr = J.writeJSON currentDbVersion
+
+  WS.setItem "db" dbStr
+  WS.setItem "last_updated" lastUpdatedStr
+  WS.setItem "db_version" currentDbVersionStr
+  where
+  toSerializable :: Db -> SerializableDb
+  toSerializable db =
+    { allWeapons: MapAsArray db.allWeapons
+    , groupedByEffect: MapAsArray db.groupedByEffect
+    , allCharacterNames: SetAsArray db.allCharacterNames
+    }
---
+++ src/Core/Database/VLatest2.purs
@@ -0,0 +1,314 @@
+module Core.Database.VLatest2 where
+
+import Prelude
+
+import Core.Display (class Display, display)
+import Data.Array.NonEmpty (NonEmptyArray)
+import Data.Generic.Rep (class Generic)
+import Data.Map (Map)
+import Data.Maybe (Maybe)
+import Data.Newtype (class Newtype, unwrap)
+import Data.Set (Set)
+import Data.Show.Generic (genericShow)
+import Data.String.NonEmpty (NonEmptyString)
+import Utils (MapAsArray, SetAsArray)
+import Utils as Utils
+import Yoga.JSON (class ReadForeign, class WriteForeign)
+import Yoga.JSON.Generics as J
+import Yoga.JSON.Generics.EnumSumRep as Enum
+import Yoga.JSON.Generics.TaggedSumRep as TaggedSum
+
+type Db =
+  { allWeapons :: Map WeaponName WeaponData
+  , groupedByEffect :: Map FilterEffectType (Array GroupedWeapon)
+  , allCharacterNames :: Set CharacterName
+  }
+
+type WeaponData =
+  { weapon :: Weapon
+  , ignored :: Boolean
+  -- List of all groups of Overboost Levels with the same weapon effect potencies.
+  --
+  -- For most weapons, this will be [OB0-5, Ob6-Ob10]
+  -- because they have the same potencies between Ob0 and Ob5,
+  -- and between Ob6 and Ob10.
+  , distinctObs :: NonEmptyArray ObRange
+  , ownedOb :: ObRange
+  }
+
+type SerializableDb =
+  { allWeapons :: MapAsArray WeaponName WeaponData
+  , groupedByEffect :: MapAsArray FilterEffectType (Array GroupedWeapon)
+  , allCharacterNames :: SetAsArray CharacterName
+  }
+
+newtype CharacterName = CharacterName NonEmptyString
+newtype WeaponName = WeaponName NonEmptyString
+
+type Weapon =
+  { name :: WeaponName
+  , character :: CharacterName
+  , source :: NonEmptyString
+  , image :: NonEmptyString
+  , atbCost :: Int
+  , ob0 :: ObLevel
+  , ob1 :: ObLevel
+  , ob6 :: ObLevel
+  , ob10 :: ObLevel
+  , cureAllAbility :: Boolean
+  }
+
+type ObRange =
+  { from :: FromOb
+  , to :: Maybe ToOb
+  }
+
+data FromOb = FromOb0 | FromOb1 | FromOb6 | FromOb10
+data ToOb = ToOb5 | ToOb9
+
+type ObLevel =
+  { description :: NonEmptyString -- ^ The source text from which the buffs/debuffs were parsed.
+  , effects :: Array WeaponEffect
+  }
+
+type GroupedWeapon =
+  { weaponName :: WeaponName
+  , range :: Range
+  -- This will be `None` for effects that don't have potencies (e.g. `Heal`)
+  , potencies :: Maybe GroupedWeaponPotencies
+  }
+
+type GroupedWeaponPotencies =
+  { ob0 :: Potencies
+  , ob1 :: Potencies
+  , ob6 :: Potencies
+  , ob10 :: Potencies
+  }
+
+data Potency
+  = Low
+  | Mid
+  | High
+  | ExtraHigh
+
+data Range
+  = All
+  | SingleTarget
+  | Self
+
+newtype Percentage = Percentage Int
+newtype Duration = Duration Int
+newtype Extension = Extension Int
+
+type DurExt =
+  { duration :: Duration
+  , extension :: Extension
+  }
+
+data EffectType
+  = Heal { percentage :: Percentage }
+  -- Buffs
+  | PatkUp { durExt :: DurExt, potencies :: Potencies }
+  | MatkUp { durExt :: DurExt, potencies :: Potencies }
+  | PdefUp { durExt :: DurExt, potencies :: Potencies }
+  | MdefUp { durExt :: DurExt, potencies :: Potencies }
+  | FireDamageUp { durExt :: DurExt, potencies :: Potencies }
+  | IceDamageUp { durExt :: DurExt, potencies :: Potencies }
+  | ThunderDamageUp { durExt :: DurExt, potencies :: Potencies }
+  | EarthDamageUp { durExt :: DurExt, potencies :: Potencies }
+  | WaterDamageUp { durExt :: DurExt, potencies :: Potencies }
+  | WindDamageUp { durExt :: DurExt, potencies :: Potencies }
+  | Veil { durExt :: DurExt, percentage :: Percentage }
+  | Provoke { durExt :: DurExt }
+  -- Debuffs
+  | PatkDown { durExt :: DurExt, potencies :: Potencies }
+  | MatkDown { durExt :: DurExt, potencies :: Potencies }
+  | PdefDown { durExt :: DurExt, potencies :: Potencies }
+  | MdefDown { durExt :: DurExt, potencies :: Potencies }
+  | FireResistDown { durExt :: DurExt, potencies :: Potencies }
+  | IceResistDown { durExt :: DurExt, potencies :: Potencies }
+  | ThunderResistDown { durExt :: DurExt, potencies :: Potencies }
+  | EarthResistDown { durExt :: DurExt, potencies :: Potencies }
+  | WaterResistDown { durExt :: DurExt, potencies :: Potencies }
+  | WindResistDown { durExt :: DurExt, potencies :: Potencies }
+  | Enfeeble { durExt :: DurExt }
+  | Stop { durExt :: DurExt }
+  | ExploitWeakness { durExt :: DurExt, percentage :: Percentage }
+
+type WeaponEffect =
+  { effectType ::
+      EffectType
+  , range ::
+      Range
+  }
+
+type Potencies =
+  { base :: Potency
+  , max :: Potency
+  }
+
+data FilterEffectType
+  = FilterHeal
+
+  | FilterVeil
+  | FilterProvoke
+  | FilterEnfeeble
+  | FilterStop
+  | FilterExploitWeakness
+
+  | FilterPatkUp
+  | FilterMatkUp
+  | FilterPdefUp
+  | FilterMdefUp
+  | FilterFireDamageUp
+  | FilterIceDamageUp
+  | FilterThunderDamageUp
+  | FilterEarthDamageUp
+  | FilterWaterDamageUp
+  | FilterWindDamageUp
+
+  | FilterPatkDown
+  | FilterMatkDown
+  | FilterPdefDown
+  | FilterMdefDown
+  | FilterFireResistDown
+  | FilterIceResistDown
+  | FilterThunderResistDown
+  | FilterEarthResistDown
+  | FilterWaterResistDown
+  | FilterWindResistDown
+
+derive instance Generic Range _
+derive instance Generic EffectType _
+derive instance Generic Potency _
+derive instance Generic FilterEffectType _
+derive instance Generic FromOb _
+derive instance Generic ToOb _
+
+derive instance Eq Range
+derive instance Eq EffectType
+derive instance Eq Potency
+derive instance Eq FilterEffectType
+derive newtype instance Eq Percentage
+derive newtype instance Eq Duration
+derive newtype instance Eq Extension
+derive newtype instance Eq CharacterName
+derive newtype instance Eq WeaponName
+
+derive instance Ord Potency
+derive instance Ord FilterEffectType
+derive newtype instance Ord CharacterName
+derive newtype instance Ord WeaponName
+
+derive instance Newtype Percentage _
+derive instance Newtype Duration _
+derive instance Newtype Extension _
+derive instance Newtype CharacterName _
+derive instance Newtype WeaponName _
+
+instance Show Range where
+  show = genericShow
+
+instance Show EffectType where
+  show = genericShow
+
+instance Show Potency where
+  show = genericShow
+
+instance Show FilterEffectType where
+  show = genericShow
+
+derive newtype instance Show Percentage
+derive newtype instance Show Duration
+derive newtype instance Show Extension
+derive newtype instance Show CharacterName
+derive newtype instance Show WeaponName
+
+instance WriteForeign Range where
+  writeImpl = J.genericWriteForeignEnum Enum.defaultOptions
+
+instance WriteForeign EffectType where
+  writeImpl = J.genericWriteForeignTaggedSum TaggedSum.defaultOptions
+
+instance WriteForeign Potency where
+  writeImpl = J.genericWriteForeignEnum Enum.defaultOptions
+
+instance WriteForeign FilterEffectType where
+  writeImpl = J.genericWriteForeignEnum Enum.defaultOptions
+
+instance WriteForeign FromOb where
+  writeImpl = J.genericWriteForeignEnum Enum.defaultOptions
+
+instance WriteForeign ToOb where
+  writeImpl = J.genericWriteForeignEnum Enum.defaultOptions
+
+derive newtype instance WriteForeign Percentage
+derive newtype instance WriteForeign Duration
+derive newtype instance WriteForeign Extension
+derive newtype instance WriteForeign CharacterName
+derive newtype instance WriteForeign WeaponName
+
+instance ReadForeign Range where
+  readImpl = J.genericReadForeignEnum Enum.defaultOptions
+
+instance ReadForeign EffectType where
+  readImpl = J.genericReadForeignTaggedSum TaggedSum.defaultOptions
+
+instance ReadForeign Potency where
+  readImpl = J.genericReadForeignEnum Enum.defaultOptions
+
+instance ReadForeign FilterEffectType where
+  readImpl = J.genericReadForeignEnum Enum.defaultOptions
+
+instance ReadForeign FromOb where
+  readImpl = J.genericReadForeignEnum Enum.defaultOptions
+
+instance ReadForeign ToOb where
+  readImpl = J.genericReadForeignEnum Enum.defaultOptions
+
+derive newtype instance ReadForeign Percentage
+derive newtype instance ReadForeign Duration
+derive newtype instance ReadForeign Extension
+derive newtype instance ReadForeign CharacterName
+derive newtype instance ReadForeign WeaponName
+
+instance Display WeaponName where
+  display = display <<< unwrap
+
+instance Display CharacterName where
+  display = display <<< unwrap
+
+instance Display FilterEffectType where
+  display = case _ of
+    FilterHeal -> "Heal"
+
+    FilterVeil -> "Veil"
+    FilterProvoke -> "Provoke"
+    FilterEnfeeble -> "Enfeeble"
+    FilterStop -> "Stop"
+    FilterExploitWeakness -> "Exploit Weakness"
+
+    FilterPatkUp -> "PATK up"
+    FilterMatkUp -> "MATK up"
+    FilterPdefUp -> "PDEF up"
+    FilterMdefUp -> "MDEF up"
+    FilterFireDamageUp -> "Fire damage up"
+    FilterIceDamageUp -> "Ice damage up"
+    FilterThunderDamageUp -> "Thunder damage up"
+    FilterEarthDamageUp -> "Earth damage up"
+    FilterWaterDamageUp -> "Water damage up"
+    FilterWindDamageUp -> "Wind damage up"
+
+    FilterPatkDown -> "PATK down"
+    FilterMatkDown -> "MATK down"
+    FilterPdefDown -> "PDEF down"
+    FilterMdefDown -> "MDEF down"
+    FilterFireResistDown -> "Fire resist down"
+    FilterIceResistDown -> "Ice resist down"
+    FilterThunderResistDown -> "Thunder resist down"
+    FilterEarthResistDown -> "Earth resist down"
+    FilterWaterResistDown -> "Water resist down"
+    FilterWindResistDown -> "Wind resist down"
+
+allFilterEffectTypes :: Array FilterEffectType
+allFilterEffectTypes = Utils.listEnum

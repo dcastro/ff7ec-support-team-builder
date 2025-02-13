816b7d075feb09eb22dc7110c9710db921845836
Diogo Castro <dc@diogocastro.com>
2025-02-13 14:50:00 +0000

    Duplicated modules

---
+++ src/Core/Database.purs
@@ -0,0 +1,260 @@
+module Core.Database where
+
+import Core.Database.VLatest
+import Prelude
+
+import Control.Monad.Error.Class (class MonadThrow, throwError)
+import Control.Monad.Except (runExceptT)
+import Control.Monad.Rec.Class (class MonadRec)
+import Core.Display (display)
+import Core.Weapons.Parser (Weapon)
+import Core.Weapons.Parser as P
+import Core.WebStorage as WS
+import Data.Array as Arr
+import Data.DateTime (DateTime)
+import Data.DateTime as DateTime
+import Data.Either (Either(..), hush)
+import Data.Foldable (for_)
+import Data.Foldable as F
+import Data.Map (Map)
+import Data.Map as Map
+import Data.Maybe (Maybe(..))
+import Data.Newtype (unwrap)
+import Data.Set (Set)
+import Data.Set as Set
+import Data.String.NonEmpty as NES
+import Data.Time.Duration (Hours(..))
+import Data.Unfoldable as Unfoldable
+import Effect.Aff (Aff)
+import Effect.Aff.Class (class MonadAff, liftAff)
+import Effect.Class (class MonadEffect, liftEffect)
+import Effect.Class.Console as Console
+import Effect.Now as Now
+import Google.SheetsApi as SheetsApi
+import Record as Record
+import Type.Proxy (Proxy(..))
+import Utils (MapAsArray(..), SetAsArray(..), logOnLeft, renderJsonErr, throwOnNothing, whenJust)
+import Yoga.JSON as J
+
+currentDbVersion :: Int
+currentDbVersion = 1
+
+type GroupEntry =
+  { filter :: Filter
+  , weaponName :: WeaponName
+  , potenciesAtOb10 :: Maybe Potencies
+  }
+
+init :: Aff (Maybe Armory)
+init = do
+  runExceptT readFromCache >>= case _ of
+    Left _ -> do
+      Console.log "Armory not found in cache, loading it from the spreadsheet..."
+      armoryMb <- hush <$> runExceptT (loadAndCreateArmory Map.empty)
+      whenJust armoryMb $ writeToCache
+      pure armoryMb
+    Right { armory, hasExpired } | hasExpired -> do
+      Console.log "Armory found in cache but has expired, updating cache..."
+      hush <$> runExceptT (loadAndCreateArmory armory.allWeapons) >>= case _ of
+        Just updatedArmory -> do
+          writeToCache updatedArmory
+          pure $ Just updatedArmory
+        Nothing -> do
+          Console.log "Failed to updated armory"
+          pure $ Just armory
+    Right { armory, hasExpired: _ } -> do
+      Console.log "Armory found in cache."
+      pure $ Just armory
+  where
+  -- Load the weapons from the spreadsheet, and updating the existing armory.
+  loadAndCreateArmory :: forall f. MonadAff f => MonadThrow Unit f => MonadRec f => Map WeaponName ArmoryWeapon -> f Armory
+  loadAndCreateArmory existingWeapons = do
+    weapons <- loadFromSpreadsheet
+    createArmory weapons existingWeapons
+
+  -- Throws if we can't parse the Google Sheet.
+  loadFromSpreadsheet :: forall f. MonadAff f => MonadThrow Unit f => f (Array Weapon)
+  loadFromSpreadsheet = do
+    table <- liftAff $ SheetsApi.getSheet "Weapons!A:Z"
+
+    let { weapons, errors } = P.parseWeapons table.result.values
+    for_ errors \err -> Console.log $ "Failed to parse weapon:\n" <> err
+
+    when (Arr.null weapons) do
+      Console.log "Failed to parse any weapons"
+      throwError unit
+    pure weapons
+
+newArmory :: Armory
+newArmory =
+  { allWeapons: Map.empty
+  , groupedByEffect: Map.empty
+  , allCharacterNames: Set.empty
+  }
+
+createArmory :: forall m. MonadEffect m => MonadRec m => Array Weapon -> Map WeaponName ArmoryWeapon -> m Armory
+createArmory newWeapons existingWeapons = do
+  Arr.foldRecM
+    (\armory weapon -> insertWeapon weapon existingWeapons armory)
+    newArmory
+    newWeapons
+
+insertWeapon
+  :: forall m
+   . MonadEffect m
+  => Weapon
+  -> Map WeaponName ArmoryWeapon
+  -> Armory
+  -> m Armory
+insertWeapon weapon existingWeapons armory =
+  case Map.lookup weapon.name existingWeapons of
+    Just existingWeapon -> do
+      Console.log $ "Weapon already exists, replacing it: " <> display weapon.name
+      pure $ armory
+        # mergeWithExisting existingWeapon
+        # insertIntoGroups
+        # insertCharacterName
+    Nothing -> do
+      Console.log $ "Weapon added: " <> display weapon.name
+      pure $ armory
+        # insert
+        # insertIntoGroups
+        # insertCharacterName
+  where
+
+  insert :: Armory -> Armory
+  insert armory = do
+    -- By default, we ignore only "Event" weapons.
+    let ignored = if NES.toString weapon.source == "Event" then true else false
+    let armoryWeapon = Record.insert (Proxy :: Proxy "ignored") ignored weapon
+    armory { allWeapons = Map.insert armoryWeapon.name armoryWeapon armory.allWeapons }
+
+  mergeWithExisting :: ArmoryWeapon -> Armory -> Armory
+  mergeWithExisting existing armory = do
+    let armoryWeapon = Record.insert (Proxy :: Proxy "ignored") existing.ignored weapon
+    armory { allWeapons = Map.insert armoryWeapon.name armoryWeapon armory.allWeapons }
+
+  insertIntoGroups :: Armory -> Armory
+  insertIntoGroups armory =
+    F.foldr
+      addToGroup
+      armory
+      matchingFilters
+
+  matchingEffectType :: EffectType -> Maybe { effectType :: FilterEffectType, potencies :: Maybe Potencies }
+  matchingEffectType = case _ of
+    Heal { percentage } ->
+      if unwrap percentage >= 35 then Just { effectType: FilterHeal, potencies: Nothing }
+      else Nothing
+
+    Veil _ -> Just { effectType: FilterVeil, potencies: Nothing }
+    Provoke _ -> Just { effectType: FilterProvoke, potencies: Nothing }
+    Enfeeble _ -> Just { effectType: FilterEnfeeble, potencies: Nothing }
+    Stop _ -> Just { effectType: FilterStop, potencies: Nothing }
+    ExploitWeakness _ -> Just { effectType: FilterExploitWeakness, potencies: Nothing }
+
+    PatkUp { potencies } -> Just { effectType: FilterPatkUp, potencies: Just potencies }
+    MatkUp { potencies } -> Just { effectType: FilterMatkUp, potencies: Just potencies }
+    PdefUp { potencies } -> Just { effectType: FilterPdefUp, potencies: Just potencies }
+    MdefUp { potencies } -> Just { effectType: FilterMdefUp, potencies: Just potencies }
+    FireDamageUp { potencies } -> Just { effectType: FilterFireDamageUp, potencies: Just potencies }
+    IceDamageUp { potencies } -> Just { effectType: FilterIceDamageUp, potencies: Just potencies }
+    ThunderDamageUp { potencies } -> Just { effectType: FilterThunderDamageUp, potencies: Just potencies }
+    EarthDamageUp { potencies } -> Just { effectType: FilterEarthDamageUp, potencies: Just potencies }
+    WaterDamageUp { potencies } -> Just { effectType: FilterWaterDamageUp, potencies: Just potencies }
+    WindDamageUp { potencies } -> Just { effectType: FilterWindDamageUp, potencies: Just potencies }
+
+    PatkDown { potencies } -> Just { effectType: FilterPatkDown, potencies: Just potencies }
+    MatkDown { potencies } -> Just { effectType: FilterMatkDown, potencies: Just potencies }
+    PdefDown { potencies } -> Just { effectType: FilterPdefDown, potencies: Just potencies }
+    MdefDown { potencies } -> Just { effectType: FilterMdefDown, potencies: Just potencies }
+    FireResistDown { potencies } -> Just { effectType: FilterFireResistDown, potencies: Just potencies }
+    IceResistDown { potencies } -> Just { effectType: FilterIceResistDown, potencies: Just potencies }
+    ThunderResistDown { potencies } -> Just { effectType: FilterThunderResistDown, potencies: Just potencies }
+    EarthResistDown { potencies } -> Just { effectType: FilterEarthResistDown, potencies: Just potencies }
+    WaterResistDown { potencies } -> Just { effectType: FilterWaterResistDown, potencies: Just potencies }
+    WindResistDown { potencies } -> Just { effectType: FilterWindResistDown, potencies: Just potencies }
+
+  matchingRanges :: Range -> Array FilterRange
+  matchingRanges = case _ of
+    Self -> [ FilterSelfOrSingleTargetOrAll ]
+    SingleTarget -> [ FilterSelfOrSingleTargetOrAll, FilterSingleTargetOrAll ]
+    All -> [ FilterSelfOrSingleTargetOrAll, FilterSingleTargetOrAll, FilterAll ]
+
+  matchingFilters' :: WeaponEffect -> Array GroupEntry
+  matchingFilters' { effectType, range } = do
+    range <- matchingRanges range
+    { effectType, potencies } <- Unfoldable.fromMaybe $ matchingEffectType effectType
+    pure
+      { filter: { effectType, range }
+      , weaponName: weapon.name
+      , potenciesAtOb10: potencies
+      }
+
+  matchingFilters :: Set GroupEntry
+  matchingFilters = do
+    let groupEntries = Set.fromFoldable $ weapon.ob10.effects >>= \effect -> matchingFilters' effect
+
+    if weapon.cureAllAbility then do
+      -- Assuming a 5* Cura materia (100% heal) with a -40% penalty
+      let healAll = { effectType: Heal { percentage: Percentage 60 }, range: All }
+      Set.union groupEntries (Set.fromFoldable $ matchingFilters' healAll)
+
+    else groupEntries
+
+  addToGroup :: GroupEntry -> Armory -> Armory
+  addToGroup { filter, weaponName, potenciesAtOb10 } armory = do
+    let
+      groupedWeapon = { weaponName, potenciesAtOb10 } :: GroupedWeapon
+      groupedByEffect = Map.alter
+        ( case _ of
+            Just weapons -> Just $ Arr.snoc weapons groupedWeapon
+            Nothing -> Just [ groupedWeapon ]
+        )
+        filter
+        armory.groupedByEffect
+    armory { groupedByEffect = groupedByEffect }
+
+  insertCharacterName :: Armory -> Armory
+  insertCharacterName armory =
+    armory { allCharacterNames = Set.insert weapon.character armory.allCharacterNames }
+
+-- Throws if the cache is empty OR the cache data is corrupted.
+readFromCache :: forall m. MonadThrow Unit m => MonadAff m => m { armory :: Armory, hasExpired :: Boolean }
+readFromCache = do
+  armoryStr <- throwOnNothing $ WS.getItem "armory"
+  lastUpdatedStr <- throwOnNothing $ WS.getItem "last_updated"
+
+  armory :: Armory <- fromSerializable <$> J.readJSON armoryStr `logOnLeft` \err ->
+    "Failed to deserialize armory:\n" <> renderJsonErr err
+  lastUpdated :: DateTime <- J.readJSON lastUpdatedStr `logOnLeft` \err ->
+    "Failed to deserialize armory:\n" <> renderJsonErr err
+
+  now <- liftEffect Now.nowDateTime
+  let hasExpired = DateTime.diff now lastUpdated > Hours 24.0
+
+  pure { armory, hasExpired }
+  where
+  fromSerializable :: SerializableArmory -> Armory
+  fromSerializable armory =
+    { allWeapons: unwrap armory.allWeapons
+    , groupedByEffect: unwrap armory.groupedByEffect
+    , allCharacterNames: unwrap armory.allCharacterNames
+    }
+
+writeToCache :: forall m. MonadAff m => Armory -> m Unit
+writeToCache armory = do
+  let armoryStr = J.writeJSON $ toSerializable armory
+  lastUpdatedStr <- J.writeJSON <$> liftEffect Now.nowDateTime
+  let currentDbVersionStr = J.writeJSON currentDbVersion
+
+  WS.setItem "armory" armoryStr
+  WS.setItem "last_updated" lastUpdatedStr
+  WS.setItem "db_version" currentDbVersionStr
+  where
+  toSerializable :: Armory -> SerializableArmory
+  toSerializable armory =
+    { allWeapons: MapAsArray armory.allWeapons
+    , groupedByEffect: MapAsArray armory.groupedByEffect
+    , allCharacterNames: SetAsArray armory.allCharacterNames
+    }
---
+++ src/Core/Weapons/Parser2.purs
@@ -0,0 +1,299 @@
+module Core.Weapons.Parser2 where
+
+import Core.Database.VLatest
+import Prelude
+
+import Control.Alt ((<|>))
+import Data.Array as Arr
+import Data.Bifunctor (lmap)
+import Data.Either (Either(..), hush)
+import Data.FoldableWithIndex as F
+import Data.Maybe (Maybe(..))
+import Data.String.NonEmpty (NonEmptyString)
+import Data.String.NonEmpty as NES
+import Data.String.Utils as String
+import Data.Tuple (Tuple(..))
+import Parsing (runParser)
+import Parsing as P
+import Parsing.Combinators as P
+import Parsing.String as P
+import Parsing.String.Basic as P
+import Utils (unsafeFromJust)
+
+type Result a = Either String a
+
+type ParseResult =
+  { weapons ::
+      Array Weapon
+  , errors :: Array String
+  }
+
+type Weapon =
+  { name :: WeaponName
+  , character :: CharacterName
+  , source :: NonEmptyString
+  , image :: NonEmptyString
+  , atbCost :: Int
+  , ob0 :: ObLevel
+  , ob1 :: ObLevel
+  , ob6 :: ObLevel
+  , ob10 :: ObLevel
+  , cureAllAbility :: Boolean
+  }
+
+parseWeapons :: Array (Array String) -> ParseResult
+parseWeapons lines =
+  lines
+    # Arr.drop 1 -- Skip the 1st row with the column headers
+    # F.foldlWithIndex
+        ( \rowIndex result row ->
+            case parseWeapon rowIndex row of
+              Right weapon -> result { weapons = Arr.snoc result.weapons weapon }
+              Left err -> result { errors = Arr.snoc result.errors err }
+        )
+        { weapons: [], errors: [] }
+
+parseWeapon :: Int -> Array String -> Result Weapon
+parseWeapon rowIndex row = do
+  name <- WeaponName <$> getCell 0
+  character <- CharacterName <$> getCell 1
+  source <- getCell 2
+  { obLevel: ob0 } <- getDescription 18
+  { obLevel: ob1 } <- getDescription 19
+  { obLevel: ob6 } <- getDescription 20
+  { obLevel: ob10, atbCost } <- getDescription 21
+  image <- getCell 22
+  -- Check if the 3rd S.Ability is a "cure all" slot
+  thirdSupportAbility <- getCell 25
+  let
+    cureAllAbility = String.startsWith "All (Cure Spells)" (NES.toString thirdSupportAbility)
+  pure
+    { name
+    , character
+    , source
+    , image
+    , atbCost
+    , ob0
+    , ob1
+    , ob6
+    , ob10
+    , cureAllAbility
+    }
+  where
+  rowId = rowIndex + 1
+
+  getDescription :: Int -> Result ParsedDescription
+  getDescription columnIndex = do
+    let
+      columnId = columnIndex + 1
+    getCell columnIndex >>= parseDescription { rowId, columnId }
+
+  getCell :: Int -> Result NonEmptyString
+  getCell columnIndex = do
+    let
+      columnId = columnIndex + 1
+    str <-
+      Arr.index row columnIndex
+        `onErr`
+          ("Row " <> show rowId <> " does not contain a cell at column " <> show columnId)
+    NES.fromString str
+      `onErr`
+        ("Cell at " <> show rowId <> ":" <> show columnId <> " is empty")
+
+infixl 0 onErr as !@
+
+onErr :: forall a. Maybe a -> String -> Either String a
+onErr ma err = case ma of
+  Just a -> Right a
+  Nothing -> Left err
+
+type Coords = { rowId :: Int, columnId :: Int }
+
+type ParsedDescription =
+  { atbCost :: Int
+  , obLevel :: ObLevel
+  }
+
+parseDescription :: Coords -> NonEmptyString -> Result ParsedDescription
+parseDescription coords description = do
+  let
+    lines = description # NES.toString # String.lines
+    firstLine = Arr.head lines `unsafeFromJust` "String.lines returned empty list"
+  let
+    effects =
+      lines
+        # Arr.mapMaybe \line -> hush $ runParser line (parseWeaponEffect coords)
+  atbCost <- runParser firstLine (parseAtbCost coords) #
+    lmap P.parseErrorMessage
+
+  pure
+    { atbCost
+    , obLevel:
+        { description
+        , effects
+        }
+    }
+
+type Parser = P.Parser String
+
+parseAtbCost :: Coords -> Parser Int
+parseAtbCost coords = do
+  inContext ("Coords " <> show coords) do
+    Tuple _ atbCost <- P.manyTill_ P.anyChar do
+      P.char '(' *> P.intDecimal <* P.string " ATB)"
+    pure atbCost
+
+parseWeaponEffect :: Coords -> Parser WeaponEffect
+parseWeaponEffect coords =
+  inContext ("Coords " <> show coords) do
+    effectType <- parseEffectType <* space
+    range <- parseRange
+    pure
+      { effectType
+      , range
+      }
+
+space :: Parser Unit
+space = void $ P.char ' '
+
+-- E.g. `30s`
+parseDuration :: Parser Duration
+parseDuration = inContext "Duration" $ Duration <$> P.intDecimal <* P.char 's'
+
+-- Some buffs (like Veil) have a percentage specifier
+-- E.g. `30%`
+parsePercentage :: Parser Percentage
+parsePercentage = inContext "Percentage" $ Percentage <$> P.intDecimal <* P.char '%'
+
+-- E.g. `(+30s)`
+parseExtension :: Parser Extension
+parseExtension =
+  inContext "Extension" do
+    Extension <$>
+      inParens do
+        (P.char '+' *> P.intDecimal <* P.char 's')
+
+{-
+
+Types of ranges:
+  * [Range: All Allies]
+  * [Range: Other Allies] (not currently handled)
+  * [Range: Single Ally]
+  * [Range: All Enemies]
+  * [Range: Single Enemy]
+  * [Range: Self]
+-}
+parseRange :: Parser Range
+parseRange =
+  inContext "Range" do
+    inBrackets do
+      _ <- P.string "Range: "
+      (P.string "All Allies" <|> P.string "All Enemies") $> All
+        <|> (P.string "Single Ally" <|> P.string "Single Enemy") $> SingleTarget
+        <|> P.string "Self" $> Self
+
+parsePotency :: Parser Potency
+parsePotency =
+  inContext "Potency" do
+    P.string "Low" $> Low
+      <|> P.string "Mid" $> Mid
+      <|> P.string "High" $> High
+      <|> P.string "Extra High" $> ExtraHigh
+
+parsePotencies :: Parser { base :: Potency, max :: Potency }
+parsePotencies =
+  inContext "Potencies" do
+    inParens $ P.try parseTwo <|> parseSingle
+  where
+  parseTwo =
+    inContext "Potency Range" do
+      base <- parsePotency
+      _ <- P.string " -> "
+      max <- parsePotency
+      pure { base, max }
+
+  parseSingle =
+    inContext "Single Potency" do
+      pot <- parsePotency
+      pure { base: pot, max: pot }
+
+-- Examples:
+-- * `PATK Down (+7s) (Low -> Mid)`
+-- * `Veil (+8s)`
+-- * `Heal`
+parseEffectType :: Parser EffectType
+parseEffectType =
+  inContext "EffectType" do
+    P.try (withPercentage "Heal" Heal)
+    <|> P.try (withDurExtPotencies "PATK Up" PatkUp)
+    <|> P.try (withDurExtPotencies "MATK Up" MatkUp)
+    <|> P.try (withDurExtPotencies "PDEF Up" PdefUp)
+    <|> P.try (withDurExtPotencies "MDEF Up" MdefUp)
+    <|> P.try (withDurExtPotencies "Fire Damage Up" FireDamageUp)
+    <|> P.try (withDurExtPotencies "Ice Damage Up" IceDamageUp)
+    <|> P.try (withDurExtPotencies "Thunder Damage Up" ThunderDamageUp)
+    <|> P.try (withDurExtPotencies "Earth Damage Up" EarthDamageUp)
+    <|> P.try (withDurExtPotencies "Water Damage Up" WaterDamageUp)
+    <|> P.try (withDurExtPotencies "Wind Damage Up" WindDamageUp)
+    <|> P.try (withDurExtPercentage "Veil" Veil)
+    <|> P.try (withDurExt "Provoke" Provoke)
+    <|> P.try (withDurExtPotencies "PATK Down" PatkDown)
+    <|> P.try (withDurExtPotencies "MATK Down" MatkDown)
+    <|> P.try (withDurExtPotencies "PDEF Down" PdefDown)
+    <|> P.try (withDurExtPotencies "MDEF Down" MdefDown)
+    <|> P.try (withDurExtPotencies "Fire Resistance Down" FireResistDown)
+    <|> P.try (withDurExtPotencies "Ice Resistance Down" IceResistDown)
+    <|> P.try (withDurExtPotencies "Thunder Resistance Down" ThunderResistDown)
+    <|> P.try (withDurExtPotencies "Earth Resistance Down" EarthResistDown)
+    <|> P.try (withDurExtPotencies "Water Resistance Down" WaterResistDown)
+    <|> P.try (withDurExtPotencies "Wind Resistance Down" WindResistDown)
+    <|> P.try (withDurExt "Enfeeble" Enfeeble)
+    <|> P.try (withDurExt "Stop" Stop)
+    -- NOTE: The effect on "Blue Daffodil Gloves" is named "WeaknessAttackUp",
+    -- but on "Bird of Prey" it's named "Exploit Weakness".
+    <|> P.try (withDurExtPercentage "WeaknessAttackUp" ExploitWeakness)
+    <|> P.try (withDurExtPercentage "Exploit Weakness" ExploitWeakness)
+  where
+  withPercentage :: String -> ({ percentage :: Percentage } -> EffectType) -> Parser EffectType
+  withPercentage effectName constructor = do
+    inContext effectName do
+      percentage <- parsePercentage <* space
+      _ <- P.string effectName
+      pure $ constructor $ { percentage }
+
+  withDurExt :: String -> ({ durExt :: DurExt } -> EffectType) -> Parser EffectType
+  withDurExt effectName constructor = do
+    inContext effectName do
+      duration <- parseDuration <* space
+      _ <- P.string effectName <* space
+      extension <- parseExtension
+      pure $ constructor $ { durExt: { duration, extension } }
+
+  withDurExtPercentage :: String -> ({ durExt :: DurExt, percentage :: Percentage } -> EffectType) -> Parser EffectType
+  withDurExtPercentage effectName constructor = do
+    inContext effectName do
+      duration <- parseDuration <* space
+      percentage <- parsePercentage <* space
+      _ <- P.string effectName <* space
+      extension <- parseExtension
+      pure $ constructor $ { durExt: { duration, extension }, percentage }
+
+  withDurExtPotencies :: String -> ({ durExt :: DurExt, potencies :: Potencies } -> EffectType) -> Parser EffectType
+  withDurExtPotencies effectName constructor = do
+    inContext effectName do
+      duration <- parseDuration <* space
+      _ <- P.string effectName <* space
+      extension <- parseExtension <* space
+      potencies <- parsePotencies
+      pure $ constructor $ { durExt: { duration, extension }, potencies }
+
+inContext :: forall a. String -> Parser a -> Parser a
+inContext context =
+  P.region \(P.ParseError message pos) ->
+    P.ParseError (context <> ": " <> message) pos
+
+inParens :: forall a. Parser a -> Parser a
+inParens = P.between (P.char '(') (P.char ')')
+
+inBrackets :: forall a. Parser a -> Parser a
+inBrackets = P.between (P.char '[') (P.char ']')
